b1246b861919732b1917489f3d4cb3e833b16269
Nilou Mostofi <nilou@km-direct.com>
2020-11-09 21:55:01 -0800

    starter code

---
+++ src/components/Landing.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header'
+import Home from './Home';
+import Work from './Work';
+import Contact from './Contact';
+
+import data from './YourData';
+
+const Landing = ()=>{
+    return(<div className="App">
+    <Header name={data.name} contactEmail = {data.contactEmail}></Header>
+      <Home name={data.landingPageName} paragraph = {data.landingPagePara} authorImage={data.landingPageImage}></Home>
+      <Work projects={data.projects}></Work>
+      <Contact contactEmail = {data.contactEmail} contactPara= {data.contactPara} socialLinks = {data.social}></Contact>
+    </div>)
+}
+
+export default Landing
